ec9449b42b83aa882a9aeaa3aed9326fca3c9053
Jamie <phillipsja97@gmail.com>
2020-03-24 18:55:01 -0500

    Add project files.

---
+++ Bangazon-Inc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bangazon_Inc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
